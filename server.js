045b127fc3dede4b8753569c815e04642705ce78
Nandan Kumar Kashyap <n363363@gmail.com>
2026-01-17 22:36:48 +0530

    added dynamic route

--- server.js
+++ server.js
@@ -17,6 +17,12 @@ app.post('/categories', (req, res) => {
   res.send('A new category has been created.');
 });
 
+app.get('/welcome/:username', (req, res) => {
+  const username = req.params.username;
+  const role = req.query.role;
+  res.send(`Welcome ${username}, your role is ${role}`);
+});
+
 app.all('*', (req, res) => {
   res.status(404).send('<h1>404 - Page Not Found</h1>');
 });
