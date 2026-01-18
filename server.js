2bac3a971fea55e814871e30fc38d2eac75752a2
Nandan Kumar Kashyap <n363363@gmail.com>
2026-01-18 10:34:54 +0530

    CRUD app

--- server.js
+++ server.js
@@ -3,23 +3,28 @@ const app = express();
 
 app.use(express.json());
 
-const productRoutes = require('./routes/productRoutes');
-const categoryRoutes = require('./routes/categoryRoutes');
-const welcomeRoutes = require('./routes/welcomeRoutes');
-const libraryRoutes = require('./routes/libraryRoutes');
-// const errorRoutes = require('./routes/errorRoutes');
+const studentRoutes = require('./routes/studentRoutes');
+const courseRoutes = require('./routes/courseRoutes');
 
-//logging middleware
+// logging middleware
 app.use((req, res, next) => {
   console.log(`${req.method} request made to ${req.url}`);
   next();
 });
 
-app.use('/products', productRoutes);
-app.use('/categories', categoryRoutes);
-app.use('/welcome', welcomeRoutes);
-app.use('/library', libraryRoutes);
-// app.use('/', errorRoutes);
+// Home route
+app.get('/', (req, res) => {
+  res.send('Welcome to the Student & Course Portal API!');
+});
+
+// Routes
+app.use('/students', studentRoutes);
+app.use('/courses', courseRoutes);
+
+// 404 handler
+app.use((req, res) => {
+  res.status(404).send('Page not found');
+});
 
 app.listen(4000, () => {
   console.log('Server is listening on port 4000');
